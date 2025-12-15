09c6424e647289a5f700c013abd884af6a94c2fb
JonasBertgen <jonas.bertgen@gmail.com>
2025-12-15 21:32:21 +0100

    add system change of lang and prepare input for line 1

--- lua/custom/plugins/tagebuch.lua
+++ lua/custom/plugins/tagebuch.lua
@@ -1,3 +1,5 @@
+-- todo: Write first Line with weekday and current date
+
 function Open_file()
   local directory = os.date '%Y_%m'
   local file = os.date '%d_%m'
@@ -6,11 +8,21 @@ function Open_file()
   vim.cmd('e ' .. full_file)
 end
 
+function Write_first_line()
+  vim.api.nvim_input('i' .. 'hello world' .. 'jjojj')
+end
+
+function Change_sway_lang_to_german()
+  os.execute 'swaymsg input type:keyboard xkb_layout "de"'
+end
+
 function Set_up_tagebuch()
   vim.fn.chdir '/home/tace/neovim_plugins/'
+  local language = os.execute 'swaymsg -t get_inputs | grep -m1 "xkb_active_layout_name"'
   Open_file()
   vim.cmd 'ZenMode'
   vim.cmd 'set spelllang=de'
+  Change_sway_lang_to_german()
   vim.cmd 'set spell'
 end
 
